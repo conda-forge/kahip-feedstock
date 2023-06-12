37a8581fb2911c66d75578aca692cdf9ea7be974
Min RK <benjaminrk@gmail.com>
2023-06-12 18:09:13 +0200

    put CMAKE_ARGS first
    who knows? :shrug:

--- recipe/build-kahip-python.sh
+++ recipe/build-kahip-python.sh
@@ -4,13 +4,13 @@ set -e
 rm -rvf misc/pymodule/pybind11/*
 
 cmake \
+  ${CMAKE_ARGS} \
   -DBUILD_SHARED_LIBS=ON \
   -DBUILDPYTHONMODULE=ON \
   -DPython3_FIND_STRATEGY=LOCATION \
   -DPython3_ROOT_DIR=${PREFIX} \
   -DPython3_EXECUTABLE=${PREFIX}/bin/python \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
-  ${CMAKE_ARGS} \
   -B build \
   .
 
