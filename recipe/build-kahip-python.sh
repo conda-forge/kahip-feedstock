bf82e9280ecee5f8a0c00a6ac412444fb2ad0d18
Min RK <benjaminrk@gmail.com>
2023-06-12 16:35:15 +0200

    try unsetting python strategy, removing bundled pybind11

--- recipe/build-kahip-python.sh
+++ recipe/build-kahip-python.sh
@@ -1,9 +1,11 @@
 set -e
 
+# remove bundled pybind11
+rm -rvf misc/pymodule/pybind11/*
+
 cmake \
   -DBUILD_SHARED_LIBS=ON \
   -DBUILDPYTHONMODULE=ON \
-  -DPython3_FIND_STRATEGY=LOCATION \
   -DCMAKE_INSTALL_PREFIX="$PREFIX" \
   ${CMAKE_ARGS} \
   -B build \
